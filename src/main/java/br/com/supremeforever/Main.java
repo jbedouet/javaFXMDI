66c0d94a96c3c3f2d8003fd6008d9253694894a9
Alex Dev <alex@alex-tech-adventures.com>
2015-11-29 22:27:09 -0500

    Add strategy selection radio controls for visual demo.

--- src/main/java/br/com/supremeforever/Main.java
+++ src/main/java/br/com/supremeforever/Main.java
@@ -2,14 +2,24 @@ package br.com.supremeforever;
 
 import br.com.supremeforever.mdi.MDICanvas;
 import br.com.supremeforever.mdi.MDIWindow;
+import br.com.supremeforever.mdi.*;
+import br.com.supremeforever.mdi.PlacementStrategy.PlacementStrategy;
 import javafx.application.Application;
 import javafx.application.HostServices;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -54,9 +64,42 @@ public class Main extends Application {
             //Add it to the container
             mdiCanvas.addMDIWindow(mdiWindow);
         });
+        
+        
+        //Buttons for placement strategies
+        ToggleGroup strategies = new ToggleGroup();
+        RadioButton defaultStrategy = new RadioButton("Default");
+        // sorry ugly names because I thought Java like PHP can have aliased class names
+        // so I will think of better names in next comits
+        defaultStrategy.setUserData(new br.com.supremeforever.mdi.PlacementStrategy.Default(mdiCanvas));
+        defaultStrategy.setToggleGroup(strategies);
+        defaultStrategy.setSelected(true);
+        
+        RadioButton cascadingStrategy = new RadioButton("Cascading");
+        cascadingStrategy.setUserData(new br.com.supremeforever.mdi.PlacementStrategy.Cascading(mdiCanvas));
+        cascadingStrategy.setToggleGroup(strategies);
+        
+        strategies.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> ov,
+                    Toggle previous, Toggle current) {
+                if(strategies.getSelectedToggle() != null) {
+                    PlacementStrategy strategy = (PlacementStrategy) strategies.getSelectedToggle().getUserData();
+                    mdiCanvas.setPlacementStrategy(strategy);
+                }
+            }
+        });
+        
         //Put it into the main pane
-        mainPane.getChildren().add(btnDefaultMdi);
-
+        HBox hBox = new HBox(
+                btnDefaultMdi,
+                defaultStrategy,
+                cascadingStrategy
+        );
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setSpacing(5);
+        mainPane.getChildren().add(hBox);
+        
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.show();
     }
